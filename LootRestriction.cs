f0116e4c63f09dd8dc8f89ee437027ce8ef39c01
Brad522 <bradley-522@hotmail.com>
2025-10-01 21:42:27 +1000

    Add project files.

---
+++ LootRestriction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using Harmony;
+
+namespace Erenshor_LootRestriction
+{
+    [BepInPlugin(ModGUID, ModDescription, ModVersion)]
+    public class LootRestriction : BaseUnityPlugin
+    {
+        internal const string ModName = "LootRestriction";
+        internal const string ModVersion = "1.0.0";
+        internal const string ModDescription = "Loot Restriction";
+        internal const string Author = "Brad522";
+        private const string ModGUID = Author + "." + ModName;
+    }
+}
